c7e9d05ce1e8b5e41d2c8b57c4b6e199c6b3ac93
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-29 21:44:34 +0700

    add newsletter section

--- src/app/paket-wisata/page.tsx
+++ src/app/paket-wisata/page.tsx
@@ -2,6 +2,7 @@ import PageHeaderSection from "@/components/sections/paket-wisata/page-header"
 import ExclusivePackagesSection from "@/components/sections/paket-wisata/exclusive-packages"
 import DestinationGallerySection from "@/components/sections/paket-wisata/destination-gallery"
 import BlogSection from "@/components/sections/home/blog-section"
+import NewsletterSection from "@/components/sections/home/newsletter-section"
 
 export default function PaketWisataPage() {
   return (
@@ -10,6 +11,7 @@ export default function PaketWisataPage() {
       <ExclusivePackagesSection />
       <DestinationGallerySection />
       <BlogSection />
+      <NewsletterSection />
     </>
   )
 }
