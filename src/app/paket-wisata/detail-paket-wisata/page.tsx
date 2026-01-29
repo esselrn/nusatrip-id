d2a97e394c51501786490dd2e432031a5a9d7abe
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-29 22:48:39 +0700

    fix detail page layout and gallery positioning

--- src/app/paket-wisata/detail-paket-wisata/page.tsx
+++ src/app/paket-wisata/detail-paket-wisata/page.tsx
@@ -3,17 +3,27 @@ import DetailLayout from "@/components/layouts/detail-layout"
 import GallerySection from "@/components/sections/paket-wisata/detail/gallery"
 import SidebarBooking from "@/components/sections/paket-wisata/detail/booking-sidebar"
 
-export default function DetailPaketWisataPage() {
+export default function DetailPaketWisata() {
   return (
-    <>
+    <main>
       <PageHeaderSection />
 
+      {/* FULL WIDTH GALLERY */}
+      <section className="max-w-[1200px] mx-auto px-6 mt-16">
+        <GallerySection />
+      </section>
+
+      {/* CONTENT + SIDEBAR */}
       <DetailLayout
         sidebar={<SidebarBooking />}
       >
-        <GallerySection />
-        {/* section lain menyusul */}
+        {/* nanti diisi:
+            - Info Paket
+            - Include / Exclude
+            - Itinerary
+            - Map
+        */}
       </DetailLayout>
-    </>
+    </main>
   )
 }
