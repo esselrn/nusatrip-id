f9440670de7bb2ae44aa20a758554bbb3b0fe66b
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-29 21:41:42 +0700

    add blog and article section

--- src/app/paket-wisata/page.tsx
+++ src/app/paket-wisata/page.tsx
@@ -1,6 +1,7 @@
 import PageHeaderSection from "@/components/sections/paket-wisata/page-header"
 import ExclusivePackagesSection from "@/components/sections/paket-wisata/exclusive-packages"
 import DestinationGallerySection from "@/components/sections/paket-wisata/destination-gallery"
+import BlogSection from "@/components/sections/home/blog-section"
 
 export default function PaketWisataPage() {
   return (
@@ -8,6 +9,7 @@ export default function PaketWisataPage() {
       <PageHeaderSection />
       <ExclusivePackagesSection />
       <DestinationGallerySection />
+      <BlogSection />
     </>
   )
 }
