54945318f9155654cf366292d308467ccc00ee64
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-01-30 15:49:02 +0700

    add destination section

--- src/app/destinasi/page.tsx
+++ src/app/destinasi/page.tsx
@@ -1,10 +1,11 @@
 import PageHeaderSection from "@/components/sections/destinasi/page-header"
+import DestinationSection from "@/components/sections/home/destination-section"
 
 export default function PaketWisataPage() {
   return (
     <>
       <PageHeaderSection />
-      
+      <DestinationSection />
     </>
   )
 }
