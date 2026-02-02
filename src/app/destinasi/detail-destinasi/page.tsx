77b7dd8cf84498155eb868d0497f6aa143b834c4
ripannugraha-alt <pklripannugraha2025@gmail.com>
2026-02-02 12:07:03 +0700

    setup detail destinasi layout and gallry

--- src/app/destinasi/detail-destinasi/page.tsx
+++ src/app/destinasi/detail-destinasi/page.tsx
@@ -1,10 +1,31 @@
 import PageHeaderSection from "@/components/sections/destinasi/detail/page-header"
+import GallerySection from "@/components/sections/destinasi/detail/gallery"
+import DetailLayout from "@/components/layouts/detail-layout"
 
-export default function PaketWisataPage() {
+export default function DetailPaketWisataPage() {
   return (
-    <>
+    <main>
+      {/* HEADER */}
       <PageHeaderSection />
-      
-    </>
+
+      {/* FULL WIDTH GALLERY */}
+      <section className="max-w-[1200px] mx-auto px-6 mt-16">
+        <GallerySection />
+      </section>
+
+      {/* CONTENT + SIDEBAR */}
+      <DetailLayout
+        sidebar={
+          <div className="space-y-6">
+            
+            
+          </div>
+        }
+      >
+        <div>
+          
+        </div>
+      </DetailLayout>
+    </main>
   )
 }
---
+++ src/components/sections/destinasi/detail/gallery.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image"
+
+const galleryImages = {
+  main: "/assets/prambanan02.jpg",
+  side: [
+    "/assets/prambanan.jpg",
+    "/assets/prambanan03.jpg",
+  ],
+}
+
+export default function GallerySection() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
+      
+      {/* MAIN IMAGE */}
+      <div className="md:col-span-2 h-[360px] relative rounded-xl overflow-hidden">
+        <Image
+          src={galleryImages.main}
+          alt="Raja Ampat"
+          fill
+          priority
+          className="object-cover"
+        />
+      </div>
+
+      {/* SIDE IMAGES */}
+      <div className="flex md:flex-col gap-4">
+        {galleryImages.side.map((src, i) => (
+          <div
+            key={i}
+            className="relative h-[172px] rounded-xl overflow-hidden"
+          >
+            <Image
+              src={src}
+              alt={`Raja Ampat Gallery ${i + 1}`}
+              fill
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </div>
+
+    </div>
+  )
+}
