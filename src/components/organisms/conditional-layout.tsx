fbc69cc8a2860df84d320f9c7b8704af61e848eb
hizharu <muhammadaprizalsanjaya@egmail.com>
2026-04-17 03:19:35 +0700

    fix: final navbar casing in conditional-layout

--- src/components/organisms/conditional-layout.tsx
+++ src/components/organisms/conditional-layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import Navbar from '@/components/organisms/navbar'
+import Navbar from '@/components/organisms/Navbar'
 import FooterSection from '@/components/organisms/footer'
 import NusaAIChat from '@/components/organisms/nusa-ai-chat'
 import { NotifProvider } from '@/contexts/notif-context'
